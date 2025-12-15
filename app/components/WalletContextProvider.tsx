11eacff25d0b8679ffe13fd005bf906aa8bd1b9a
Kartik Vyas <vkartik013@gmail.com>
2025-12-16 00:21:50 +0530

    fix: Correct import statement for wallet adapter styles in WalletContextProvider

--- app/components/WalletContextProvider.tsx
+++ app/components/WalletContextProvider.tsx
@@ -6,8 +6,7 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
-
-require('@solana/wallet-adapter-react-ui/styles.css');
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
